e94a59c45d751d1268bf54abf90d352d87a75682
Patric Jansson <paddy@kth.se>
2019-10-04 13:18:10 +0200

    Coding for the sake of coding

--- index.js
+++ index.js
@@ -1,22 +1,81 @@
-var fetch = require('node-fetch')
+var fetch = require("node-fetch");
 
-async function _validate(schemaPath, jsonToValidate) {
-    const defaultHost = 'https://app.kth.se/jsonschema'
-    const furanoHost = process.env.FURANO_HOST || defaultHost;
-    if (!schemaPath.startsWith('/')) schemaPath = '/' + schemaPath;
-    const validationUrl = furanoHost + schemaPath;
-    var response = await fetch(validationUrl, {
-        method: 'POST',
-        body: JSON.stringify(jsonToValidate),
-        headers: { 'Content-Type': 'application/json' }
-    }).catch(err => { throw err });
-    var jsonBody = await response.json()
-        .catch(err => { throw err });
-    if (jsonBody && response.status !== 200) {
-        return { valid: false, error: JSON.stringify(jsonBody) }
-    } else {
-        return { valid: true, error: null }
+/**
+ * Get the url to the validation service.
+ * If the env FURANO_HOST is set, this value is used.
+ */
+const getServiceUrl = () => {
+  return process.env.FURANO_HOST || "https://app.kth.se/jsonschema";
+};
+
+/**
+ * Gets the full path pointing at the schema to validate against.
+ * @param {*} schemaPath Path to the json schema relative the service url.
+ * ex: "/my-app/schema/"
+ * ex: "/flottsbro-api/deployment/"
+ */
+const getUrl = schemaPath => {
+  if (schemaPath.startsWith("/")) {
+    return getServiceUrl() + schemaPath;
+  }
+  return getServiceUrl() + "/" + schemaPath;
+};
+
+/**
+ * Get a configuration for the POST to the Furano schema validation serivce.
+ * @param {*} jsonToValidate The json to validate.
+ */
+const getPostData = jsonToValidate => {
+  return {
+    method: "POST",
+    body: JSON.stringify(jsonToValidate),
+    headers: { "Content-Type": "application/json" }
+  };
+};
+
+/**
+ * Gets a result json data strucure.
+ * @param {*} valid Mark as valid json.
+ * @param {*} validationError Any validation error, default to null.
+ */
+const result = (valid, validationError = null) => {
+  let error = null;
+  if (validationError) {
+    error = JSON.stringify(validationError);
+  }
+  return {
+    valid: valid,
+    error: error
+  };
+};
+
+/**
+ * Validate json against a schema published under a path.
+ * furano-npm.validate('app/schema', {'my': 'data'})
+ * @param {*} schemaPath Path to the json schema relative the service url.
+ * @param {*} jsonToValidate The json to validate.
+ */
+const validate = async (schemaPath, jsonToValidate) => {
+  let post = await fetch(getUrl(schemaPath), getPostData(jsonToValidate)).catch(
+    err => {
+      throw err;
     }
-}
+  );
 
-exports.validate = _validate;
+  let validationError = await post.json().catch(err => {
+    throw err;
+  });
+
+  if (validationError && post.status !== 200) {
+    return result(false, validationError);
+  } else {
+    return result(true);
+  }
+};
+
+/**
+ * Public exports
+ */
+module.exports = {
+  validate: validate
+};
